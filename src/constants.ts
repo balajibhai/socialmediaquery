2b92d742e884b6a42bdcdc60699541c0ceae6d9e
Balaji <balaji1bajji@gmail.com>
2025-05-22 12:03:57 -0400

    constants variables changed to caps

--- src/constants.ts
+++ src/constants.ts
@@ -3,15 +3,12 @@ import GraphComponent from "./components/molecules/GraphComponent";
 import TableComponent from "./components/molecules/TableComponent";
 
 export const componentMap = {
-  table: TableComponent,
-  graph: GraphComponent,
-  text: TextComponent,
+  TABLE: TableComponent,
+  GRAPH: GraphComponent,
+  TEXT: TextComponent,
 };
 
 export const MessageComponent = {
-  TABLE: "table",
-  TEXT: "text",
-  GRAPH: "graph",
   TAB: "tab",
   SET: "set",
 };
